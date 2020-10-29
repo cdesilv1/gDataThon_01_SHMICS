72b096e7cc86ef8c65be42dd58b105c0b1e45217
connorslagle <connor.slagle@colorado.edu>
2020-10-29 11:16:56 -0600

    pipeline working, all but no mentions

--- src/pipeline_to_pandas.py
+++ src/pipeline_to_pandas.py
@@ -28,7 +28,15 @@ class pipelineToPandas():
             lang = 'en'
         '''
 
-    def clean_df(self, all=True):
+    def load_json(self, fname):
+        '''
+        Loads json file to pandas df
+
+        INPUT: fname <str>: File name
+        '''
+        self.df_all = pd.read_json(f'{self.base_path}{fname}', lines=True)
+
+    def clean_df(self):
         '''
         Clean df. This includes,
             dropping duplicate rows (based on tweet_id)
@@ -36,33 +44,39 @@ class pipelineToPandas():
             format source from html to iPhone, Android, Web, Other
             format as datetime
         '''
-        if all:
-            # dropping duplicates
-            self.df_all.drop_duplicates(subset='tweet_id', ignore_index=True, inplace=True)
 
-            # Eliminating mentions, dropping 
-            range_as_tuple = self.df_all['tweet_text_range'].apply(self._format_text_range) 
-            self.df_all['tweet_text_wo_mentions'] = self._no_mentions_text(
-                range_as_tuple,
-                self.df_all['tweet_text']
-            )
-            self.df_all.drop(['tweet_text', 'tweet_text_range'], axis=1, inplace=True)  
+        # Select subset of columns
 
-            # formatting source
-            self.df_all['source_text'] = self.df_all['source'].apply(lambda x: BeautifulSoup(x).find('a').getText())
-            self.df_all.drop('source', axis=1, inplace=True)
-            source_class_dict = {'Twitter for iPhone': 'iPhone', 'Twitter for Android': 'Android',
-                     'Twitter Web App': 'Web', 'Twitter for iPad': 'iPad'}
-            self.df_all['source_text'] = self.df_all['source_text'].replace(source_class_dict)
-            self.df_all['source_text'].where(
-                self.df_all['source_text'].apply(lambda x: x in source_class_dict.values()),
-                'Other',
-                inplace=True
-            )
+        # dropping duplicates
+        self.df_all.drop_duplicates(subset='id', ignore_index=True, inplace=True)
 
-            # format datetime cols
-            self.df_all['tweet_date_created'] = pd.to_datetime(self.df_all['tweet_date_created'])
-            self.df_all['user_date_created'] = pd.to_datetime(self.df_all['user_date_created'])
+        # Eliminating mentions, dropping 
+        range_as_tuple = self.df_all['display_text_range'].apply(self._format_text_range) 
+        self.df_all['tweet_text_wo_mentions'] = self._no_mentions_text(
+            range_as_tuple,
+            self.df_all['full_text']
+        )
+        self.df_all.drop(['display_text_range'], axis=1, inplace=True)  
+
+        # formatting source
+        self.df_all['source_text'] = self.df_all['source'].apply(lambda x: self._get_atag_text(x))
+        self.df_all.drop('source', axis=1, inplace=True)
+        source_class_dict = {'Twitter for iPhone': 'iPhone', 'Twitter for Android': 'Android',
+                    'Twitter Web App': 'Web', 'Twitter for iPad': 'iPad'}
+        self.df_all['source_text'] = self.df_all['source_text'].replace(source_class_dict)
+        self.df_all['source_text'].where(
+            self.df_all['source_text'].apply(lambda x: x in source_class_dict.values()),
+            'Other',
+            inplace=True
+        )
+
+        # format datetime cols
+        self.df_all['tweet_date_created'] = pd.to_datetime(self.df_all['created_at'])
+
+    def _get_atag_text(self, tweet_text):
+        a_tag = BeautifulSoup(tweet_text).find('a')
+        if type(a_tag) == str:
+            return a_tag.getText()
 
     def _format_text_range(self, raw_range):
         '''
@@ -74,6 +88,8 @@ class pipelineToPandas():
         else:
             return raw_range
 
+    # Doesn't work yet 10/29
+    
     def _no_mentions_text(self, tup_range_series, raw_tweet_text_series):
         wo_mentions = np.empty(raw_tweet_text_series.shape, dtype='U256')
         for idx, tup in enumerate(tup_range_series):
@@ -83,19 +99,16 @@ class pipelineToPandas():
                 wo_mentions[idx] = raw_tweet_text_series[idx]
         return wo_mentions
     
-    def save_to_csv(self, csv_file_name, all=True):
-        now_date = str(datetime.now()).split(' ')[0]
-        file_path = f'{self.base_path}{now_date}_{csv_file_name}'
-        if all:
-            self.df_all.to_csv(file_path, index=False)
-        else:
-            self.pandas_df.to_csv(file_path, index=False)
+    # def save_to_csv(self, csv_file_name, all=True):
+    #     now_date = str(datetime.now()).split(' ')[0]
+    #     file_path = f'{self.base_path}{now_date}_{csv_file_name}'
+    #     if all:
+    #         self.df_all.to_csv(file_path, index=False)
+    #     else:
+    #         self.pandas_df.to_csv(file_path, index=False)
         
 
 if __name__ == "__main__":
-
-    print('hi')
     pipeline = pipelineToPandas()
-    pipeline.load_multiple_files()
+    pipeline.load_json('concatenated_abridged.jsonl')
     pipeline.clean_df()
-    pipeline.save_to_csv('all_clean.csv')
