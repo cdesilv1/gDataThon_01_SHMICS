4342e5ac68e47ece8fc2b241b703315e271df229
connorslagle <connor.slagle@colorado.edu>
2020-11-03 09:19:26 -0700

    working alpha

--- src/dataCleaningPipeline.py
+++ src/dataCleaningPipeline.py
@@ -1,6 +1,7 @@
 # Python builtin libraries
 from datetime import datetime
 import re
+import os
 
 # External libraries
 import pandas as pd
@@ -57,12 +58,13 @@ class tweetCleaner():
         self.chunk_size = chunk_size
 
         if chunk:
+            self.num_chunks = (sum(1 for row in open(f'{self.base_path}{fname}', 'r')) // chunk_size) + 1
             self.df_chunk_iter = pd.read_json(f'{self.base_path}{fname}', lines=True, chunksize=chunk_size)
 
         else:
             self.df_raw = pd.read_json(f'{self.base_path}{fname}', lines=True)
     
-    def clean_df(self):
+    def clean_df(self, proc_file_dir):
         '''
         Remove unwanted fields from tweets, add engineered features.
         '''
@@ -83,57 +85,62 @@ class tweetCleaner():
                 chunk = self._partisan_score(chunk)
 
                 # Filter tweets to subpopulations
-
+                self._write_to_subpopulations(chunk, proc_file_dir)
 
                 # print update
-                print(f'Cleaning chunks:\t{chunk_id+1} of {(self.chunk_size // len(chunk))+1} clean')
+                print(f'Cleaning chunks:\t{chunk_id+1} of {self.num_chunks} clean')
 
         else:
             self.df_raw = self._subset_tweets(self.df_raw)
             
             self.df_raw = self._sentiment_score_tweets(self.df_raw)
 
-            self.df_raw = self._id_attack_tweets(self.df_raw)
+            # self.df_raw = self._id_attack_tweets(self.df_raw)
 
             self.df_raw = self._partisan_score(self.df_raw)
 
+            self._write_to_subpopulations(self.df_raw, proc_file_dir)
 
-    def _write_to_subpopulations(self, df, partisan_thresh=1, sentiment_thresh=0.5):
+
+    def _write_to_subpopulations(self, df, proc_file_dir, sentiment_thresh=0.5):
         '''
         Filter DF by partisanship and sentiment: (populations subject to change)
             - Population 1 (proTrump): proTrump/positiveSent & proBiden/negativeSent
             - Population 2 (proBiden): proTrump/negativeSent & proBiden/positiveSent
-            - Population 3 (proTrump Attack): proTrump/attack
-            - Population 4 (proBiden Attack): proBiden/attack
         '''
         # make masks
-        proTrump_mask = df['partisan_score'] > partisan_thresh
+        proTrump_mask = df['partisan_score'] == 1
         posSent_mask = df['vader_sentiment'] > sentiment_thresh
 
-        proBiden_mask = df['partisan_score'] < -partisan_thresh
+        proBiden_mask = df['partisan_score'] == 0
         negSent_mask = df['vader_sentiment'] < -sentiment_thresh
 
         # Population combos
         mask_dict = {
-            'proTrump': (proTrump_mask & posSent_mask) & (proBiden_mask & negSent_mask),
-            'proBiden': (proTrump_mask & negSent_mask) & (proBiden_mask & posSent_mask),
+            'proTrump': (proTrump_mask & posSent_mask) | (proBiden_mask & negSent_mask),
+            'proBiden': (proTrump_mask & negSent_mask) | (proBiden_mask & posSent_mask),
         }
-
 
         '''
         To finish pipeline, 1/1, 6:30pm, Connor:
             - Add Trevor model to classify proTrump/proBiden
             - Export subpopulations as json files to GH
         '''
-        
+
         # Write to jsonl files
+        for population, mask in mask_dict.items():
+            fname = f'{population}_sentiment_thresh_{sentiment_thresh}.jsonl'
+            df_copy = df[mask]
+            with open(f'{proc_file_dir}/{fname}', 'a') as f:
+                f.writelines(df_copy.to_json(orient='records', lines=True))
+            
 
     def _partisan_score(self, df):
         '''
         Apply algoritm to score tweet partisanship.
         '''
         # Placeholder for now - replace with ML algo later (11/1, 3:20pm MDT)
-        df['partisan_score'] = np.random.randint(-10,10, size=(len(df),1))
+        df['partisan_score'] = np.random.randint(0,1, size=(len(df),1))
         return df
 
     def _id_attack_tweets(self, df):
@@ -149,7 +156,7 @@ class tweetCleaner():
         Apply VADER algo to tweet text, returns only compount score
         '''
         analyzer = SentimentIntensityAnalyzer()
-        df['vader_sentiment'] = df.apply(lambda x: (analyzer.polarity_scores(x)).get('compound'))
+        df['vader_sentiment'] = df['full_text'].apply(lambda x: (analyzer.polarity_scores(x)).get('compound'))
         return df
 
     def _subset_tweets(self, df):
@@ -157,7 +164,7 @@ class tweetCleaner():
         Subsets columns, selects english, drops duplicates, grabs user_desc & hashtags for each tweet
         '''
 
-        cols_to_keep = ['id', 'full_text', 'source', 'entities', 'user', 'lang']
+        cols_to_keep = ['id', 'full_text', 'entities', 'user', 'lang']
 
         # Subset columns
         df = df[cols_to_keep]
@@ -174,9 +181,12 @@ class tweetCleaner():
         # Grab hashtags
         df['hashtags'] = [df['entities'][ind].get('hashtags') for ind in range(len(df))]
 
+        df.drop(['user', 'lang', 'entities'], axis=1, inplace=True)
+
         return df
         
 
 if __name__ == "__main__":
+    pipeline = tweetCleaner()
     pipeline.load_json('concatenated_abridged.jsonl')
-    pipeline.clean_df()
+    pipeline.clean_df('../data/proc_jsons')
