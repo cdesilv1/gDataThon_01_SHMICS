28efda06a09872a4440ce79c9b3a592cd58dcfc0
cdesilv1 <chris.desilva_@outlook.com>
2020-11-12 17:35:22 -0700

    changes to bot, removing unneeded content

--- bot_scripts/tweet.py
+++ bot_scripts/tweet.py
@@ -14,7 +14,7 @@ logger = logging.getLogger()
 
 
 def get_fnames_helper() -> list:
-    # get fnames from helper
+    # get filenames of pre-generated GPT outputs from count_logger.json
     with open("count_logger.json", "r") as fread:
         read_json = json.load(fread)
         fnames = list(read_json.keys())
@@ -22,6 +22,9 @@ def get_fnames_helper() -> list:
 
 
 def make_tweet(api, filenames: list = get_fnames_helper()):
+    """
+    Select random candidate, select tweet based on next index to use, and post to Twitter account
+    """
     logger.info("Deciding on tweet type to post")
 
     # pop to cycle through files if one has been iterated through completely
@@ -48,20 +51,26 @@ def make_tweet(api, filenames: list = get_fnames_helper()):
 
                     def _recursive_helper(
                         read_json,
-                        next_idx_current_file,
-                        recursive_iterator=0,
-                        idx_max=5000,
+                        next_idx_current_file: str,
+                        recursive_iterator: int = 0,
+                        idx_max: int = 5000,
                     ):
+                        """
+                        Some pre-generated tweets don't have viable samples, so we are reading through tweets to find the next viable one
+                        """
+
+                        # RE pattern to find the first viable sample, as under certain conditions in gpt2_simple,
+                        # generate function does not return prefix and suffix for all individual tweets in single sample
                         find_bw_re = r"\<\|startoftext\|\>(.*?)\<\|endoftext\|\>"
                         tweet_content = read_json[
-                            str(next_idx_current_file + recursive_iterator)
+                            str(int(next_idx_current_file) + recursive_iterator)
                         ]
                         found_content = re.findall(find_bw_re, tweet_content)
-                        if next_idx_current_file + recursive_iterator <= idx_max:
+                        if int(next_idx_current_file) + recursive_iterator <= idx_max:
                             if found_content:
                                 return (
                                     found_content[0],
-                                    next_idx_current_file + recursive_iterator,
+                                    int(next_idx_current_file) + recursive_iterator,
                                 )
                             else:
                                 return _recursive_helper(
@@ -70,13 +79,17 @@ def make_tweet(api, filenames: list = get_fnames_helper()):
                                     recursive_iterator=recursive_iterator + 1,
                                 )
                         else:
-                            return "Sorry, we're out of fresh tweets.", 5000
+                            return (
+                                "Sorry, we're out of fresh tweets.",
+                                5000,
+                            )  # hardcoded end-index of tweets
 
                     tweet_content, current_idx = _recursive_helper(
                         read_json=read_json, next_idx_current_file=next_idx_current_file
                     )
                     found_new_tweet = True
                 except:
+                    # if this condition has been met, we are out of indexes for a single candidate
                     logger.info("file: {} is out of tweets".format(current_f))
 
             if found_new_tweet:
@@ -85,11 +98,14 @@ def make_tweet(api, filenames: list = get_fnames_helper()):
                 with open("count_logger.json", "w") as fwrite:
                     fwrite.write(json.dumps(read_json_ct_logger))
 
+                # returning next tweet to post
                 return tweet_content
 
         else:
+            # if this condition has been reached, we are out of indexes for both candidates
             tweet_content = "Sorry, we're out of fresh tweets."
 
+        # returning message for when out of tweets
         return tweet_content
 
     logger.info("Updating tweet")
@@ -103,6 +119,7 @@ def main():
     api = create_api()
     while True:
         make_tweet(api)
+        # randomizing post time to avoid automated Twitter account flagging for spam
         sleep_time = random.randint(2400, 3600)
         time.sleep(sleep_time)
 
